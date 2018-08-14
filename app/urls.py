0833cff997fcd379a8aaab41444ec952ec37ff79
Venkatesh Prasad <prasad.venkatesh90@gmail.com>
2018-08-14 14:27:23 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,3 @@
-"""diff_match_patch URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
@@ -21,5 +6,9 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$',views.home),
-    url(r'^dmp/$', views.diff_match_patch),
+    url(r'^d1/$', views.doc1),
+    url(r'^d2/$', views.doc2),
+    url(r'^doc1.html$', views.html1),
+    url(r'^doc2.html$', views.html2),
+
 ]
